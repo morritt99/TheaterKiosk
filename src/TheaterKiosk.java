4ffe47b118f2104e51bc0ea1c6d7fb88656001e2
morritt99 <morritt@mail.uc.edu>
2024-06-10 20:17:08 -0400

    Commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,20 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        Scanner AgeInput = new Scanner(System.in);
+        System.out.println(" \nWelcome to the theater, Kiosk Enter Here !");
+        int UserAge = 0;
+
+        // Output your age
+        System.out.println("Please Enter your age: ");
+        if (UserAge >= 21) {
+            System.out.println("\nYou get a wrist band!");
+        }
+        }
+
+    }
